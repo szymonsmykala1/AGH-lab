686877210efa11ea28ade9167b9e88fa4c10adc4
Szymon Smykała <szymon.smykala@relativity.com>
2022-10-25 10:51:13 +0200

    updated main page

--- WebApplication7_test/Program.cs
+++ WebApplication7_test/Program.cs
@@ -3,6 +3,6 @@ var app = builder.Build();
 
 var configuration = (IConfiguration) app.Services.GetService(typeof(IConfiguration))!;
 
-app.MapGet("/", () => $"Hello World! ${configuration.GetSection("MyKey").Value}");
+app.MapGet("/", () => $"Hello World! xD${configuration.GetSection("MyKey").Value}");
 
 app.Run();
