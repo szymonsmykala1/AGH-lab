238e9fdb344add5ecdd6098d7cb64f36e42889b8
Szymon Smykała <szymon.smykala@relativity.com>
2022-10-25 11:42:19 +0200

    added test project

--- WebApplication7_test/Program.cs
+++ WebApplication7_test/Program.cs
@@ -3,6 +3,7 @@ var app = builder.Build();
 
 var configuration = (IConfiguration) app.Services.GetService(typeof(IConfiguration))!;
 
-app.MapGet("/", () => $"Hello World! xD${configuration.GetSection("MyKey").Value}");
+// app.MapGet("/", () => $"Hello World! xD${configuration.GetSection("MyKey").Value}");
+app.MapGet("/", () => $"FAILING TEST");
 
 app.Run();
